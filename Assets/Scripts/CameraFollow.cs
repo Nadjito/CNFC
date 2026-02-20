49f2eb1f966dc97edfc216ad4a8c3ba92a4196fe
David Santos <davidmiguel.santos@outlook.com>
2026-02-20 12:12:36 +0000

    Camera Fixes
    Changed Camera to run on fixed update, like the penguin, to fix jittering
    
    Added Camera Rotation for better viewing angle

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,8 +5,14 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     public float smooth = 4f;
     public Vector3 offset = new Vector3(4, 0, -10);
+    public float cameraRotation;
 
-    void LateUpdate()
+    void Start()
+    {
+        transform.rotation = Quaternion.AngleAxis(cameraRotation, Vector3.right);
+    }
+
+    void FixedUpdate()
     {
         Vector3 vector = target.position + offset;
         transform.position = Vector3.Lerp(transform.position, vector, smooth * Time.deltaTime);
