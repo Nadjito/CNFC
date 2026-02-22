774feb2451f87597325df6194fcaacc675df15a9
TheShopSoy <joaovascoteodoro@gmail.com>
2026-02-22 16:33:05 +0000

    RelfexPengGoodNow

--- Assets/Scripts/PenguinController.cs
+++ Assets/Scripts/PenguinController.cs
@@ -34,7 +34,7 @@ public class PenguinController : MonoBehaviour
     private bool perfectPress;
     private bool wasOnAir;
     public Animator animator;
-    public Animator reflexAnim;
+    public Animator animatorRef;
 
     void Start()
     {
@@ -44,9 +44,8 @@ public class PenguinController : MonoBehaviour
         rb.linearVelocity = new Vector3(currentForwardSpeed, 0f, 0f);
         pressStartTime = -999f;
         animator = GetComponentInChildren<Animator>();
-        reflexAnim = GetComponentInChildren<Animator>();
-        if (animator == null)
-            Debug.LogWarning("Animator component not found on PenguinController object.");
+        animatorRef = transform.Find("PengRelfex").GetComponent<Animator>();
+
     }
 
     void Update()
@@ -111,6 +110,7 @@ public class PenguinController : MonoBehaviour
                 rb.AddForce(Vector3.up * impulse, ForceMode.Impulse);
                 currentForwardSpeed = Mathf.Min(currentForwardSpeed + speedBoostPerfect, forwardSpeed * 3f);
                 animator.SetTrigger("Parry");
+                animatorRef.SetTrigger("ParryRef");
                 Debug.Log("Parry");
                 hadPressed = false;
                 canJump = false;
